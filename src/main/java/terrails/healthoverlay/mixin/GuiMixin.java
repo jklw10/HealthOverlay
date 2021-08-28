1b24763c5e39aa6678e42c9b7a18178963e90632
Terrails <terrails@outlook.com>
2021-08-28 17:39:58 +0200

    Fixes #22

--- src/main/java/terrails/healthoverlay/mixin/GuiMixin.java
+++ src/main/java/terrails/healthoverlay/mixin/GuiMixin.java
@@ -2,10 +2,12 @@ package terrails.healthoverlay.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import terrails.healthoverlay.HealthOverlay;
 import terrails.healthoverlay.HeartRenderer;
 
 @Mixin(Gui.class)
@@ -18,6 +20,18 @@ public abstract class GuiMixin {
             at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Mth;ceil(F)I", ordinal = 0))
     private int runDefaultRenderer(float defaultValue) {
         return -1;
+    }
+
+    @Redirect(method = "renderPlayerHealth",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;getAttributeValue(Lnet/minecraft/world/entity/ai/attributes/Attribute;)D", ordinal = 0))
+    private double limitMaxHealth(Player player, Attribute attribute) {
+        return Math.min(player.getAttributeValue(attribute), 20);
+    }
+
+    @Redirect(method = "renderPlayerHealth",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;getAbsorptionAmount()F", ordinal = 0))
+    private float limitAbsorption(Player player) {
+        return HealthOverlay.absorptionOverHealth ? 0 : Math.min(player.getAbsorptionAmount(), 20);
     }
 
     @Inject(method = "renderPlayerHealth",
